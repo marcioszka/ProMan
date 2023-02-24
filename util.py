ab8b3be3558f99eb729a655e741236d2a9b227ff
marcioszka <monkiewicz.marta@gmail.com>
2023-02-24 16:12:33 +0100

    Define function to hash password

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
+import bcrypt
 from functools import wraps
-from flask import jsonify
+from flask import jsonify, session
 
 
 def json_response(func):
@@ -13,3 +14,9 @@ def json_response(func):
         return jsonify(func(*args, **kwargs))
 
     return decorated_function
+
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password.decode('utf-8')
+
