dcc993bd72336be49dca9f0a19af31ff42b63558
marcioszka <monkiewicz.marta@gmail.com>
2023-03-01 16:05:20 +0100

    Define function to add columns

--- static/js/controller/boardsManager.js
+++ static/js/controller/boardsManager.js
@@ -18,7 +18,10 @@ export let boardsManager = {
             domManager.addEventListener(`.board-header[data-board-id="${board.id}"]`,
                 "click",
                 toggleHideShowBoardColumns);
-            //domManager.addEventListener()
+            //domManager.addEventListener();
+            domManager.addEventListener(`.add-column-button[data-board-id="${board.id}"]`,
+                'click',
+                addColumn);
         }
     },
     createBoard: function () {
@@ -36,14 +39,13 @@ export let boardsManager = {
     //     domManager.addChild(".user-panel", inputBoxAndButton);
     //     let parent = element.parentElement;
     //     parent.replaceChild(newElement, element);
-    // }
+    // },
 };
 
 function deleteBoard(clickEvent) {
     const boardId = clickEvent.target.dataset.boardId;
     dataHandler.deleteBoard(boardId);
 }
-
 
 function showHideButtonHandler(clickEvent) {
     const boardId = clickEvent.target.dataset.boardId;
@@ -65,4 +67,21 @@ function addBoard(clickEvent) {
     const newBoardName = clickEvent.target.previousElementSibling.value;
     dataHandler.createNewBoard(newBoardName);
     document.getElementById('input-box').remove();
+}
+
+function addColumn(clickEvent) {
+    const columnNameInput = document.createElement('input');
+    columnNameInput.setAttribute('id', 'add-column-name-box');
+    columnNameInput.setAttribute('required', 'true');
+    const addColumnButton = clickEvent.target;
+    addColumnButton.insertAdjacentElement('afterend', columnNameInput);
+    columnNameInput.onblur = function(event){
+        let newColumnName = columnNameInput.value;
+        console.log(newColumnName);
+        const columnBuilder = htmlFactory(htmlTemplates.column);
+        const content = columnBuilder(newColumnName);
+        const boardId = parseInt(clickEvent.target.dataset.boardId);
+        domManager.addChild(`.board-columns[data-board-id="${boardId}"]`, content);
+        document.getElementById('add-column-name-box').remove();
+    }
 }
