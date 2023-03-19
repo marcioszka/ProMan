7232758bb154a7fc64c041c2826dd2f579b523b8
marcioszka <monkiewicz.marta@gmail.com>
2023-03-19 01:13:54 +0100

    Check

---
+++ static/js/controller/columnsManager.js
@@ -0,0 +1,73 @@
+import {dataHandler} from "../data/dataHandler.js";
+import {htmlFactory, htmlTemplates} from "../view/htmlFactory.js";
+import {domManager} from "../view/domManager.js";
+import { cardsManager } from "./cardsManager.js";
+
+const columnKeys= ["new", "in progress", "testing", "done"]
+
+export let columnsManager = {
+    loadColumns: async function (boardId) {
+        const columns = await dataHandler.getStatuses(boardId);
+        for (let column of columns) {
+            const columnBuilder = htmlFactory(htmlTemplates.column);
+            const content = columnBuilder(column);
+            domManager.addChild(`.columns-in-boards[data-board-id="${boardId}"]`, content);
+            domManager.addEventListener(`.delete-column-button[data-board-id="${boardId}"]`,
+               'click',
+               deleteColumn);
+            domManager.addEventListener(`.board-column-title[data-id="${column.id}"]`,
+                'click',
+                renameColumn);
+            }
+        for(let i=1; i<=4; i++){columns.push({"board_id": boardId, "id": i, "title": columnKeys[i-1]})};
+        for(let eachColumn of columns){
+            await cardsManager.loadCards(boardId, eachColumn.id);
+        }
+    },
+};
+
+function deleteColumn(clickEvent) {
+    const columnName = clickEvent.target.previousElementSibling.innerText;
+    const boardId = parseInt(clickEvent.target.dataset.boardId);
+    dataHandler.deleteColumn(columnName, boardId);
+}
+
+function renameColumn(clickEvent) {
+    clickEvent.stopPropagation();
+    const oldName = clickEvent.target.innerText;
+    clickEvent.target.style.display = "none";
+    const newColumnNameField = document.createElement('input');
+    newColumnNameField.setAttribute('id', 'change-column-name-box');
+    newColumnNameField.setAttribute('required', 'true');
+    newColumnNameField.setAttribute('placeholder', oldName);
+    clickEvent.target.insertAdjacentElement('afterend', newColumnNameField);
+    newColumnNameField.onblur = function (event) {
+        let newColumnName = newColumnNameField.value;
+        const columnId = parseInt(clickEvent.target.dataset.id);
+        dataHandler.renameColumn(columnId, newColumnName);
+        document.getElementById('change-column-name-box').remove();
+        location.reload();
+        }
+    }
+
+function handleKeyPress(keyEvent) {
+    keyEvent.stopPropagation();
+    const newName = keyEvent.target.value;
+    if(keyEvent.key == 'Enter' && newName != "")
+    {
+        const boardId = parseInt(keyEvent.target.dataset.boardId);
+        dataHandler.renameColumn(newName, columnId);
+    }
+    else if (keyEvent.key == 'Escape'){
+        return;
+    }
+}
+ async function columnNameValidation(columnName, boardId){
+    let columns = await dataHandler.getStatuses(boardId);
+    let columnNames = ["new", "in progress", "testing", "done"];
+    for(let column of columns){
+        columnNames.push(column.title);
+    }
+    columnName.toLowerCase();
+    return columnNames.includes(columnName)
+}
